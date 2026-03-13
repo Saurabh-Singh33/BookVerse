832d3fbef54fe525cff4f06e989b3d669c516d64
Saurabh-Singh33 <singhsaurabh43431@gmail.com>
2026-03-13 13:10:49 +0530

    Resolved the file space

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Home from "./home/Home";
 import { Route, Routes } from "react-router-dom";
+import Home from "./home/Home";
 import Courses from "./courses/Courses";
 import Signup from "./components/Signup";
 import { Toaster } from "react-hot-toast";
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="dark:bg-slate-900 dark:text-white">
       <Routes>
-        <Route path="/" element={<Home />} />
+       <Route path="/" element={<Home />} />
         <Route path="/course" element={<Courses />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
