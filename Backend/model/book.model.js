1cf1f586d9de81c33fff75190f1848e2d0457c6c
Saurabh-Singh33 <singhsaurabh43431@gmail.com>
2026-04-29 18:17:26 +0530

    chnages in book.model.js

--- Backend/model/book.model.js
+++ Backend/model/book.model.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
 const bookSchema = mongoose.Schema({
+    id: String,
     name: String,
     price: Number,
     category: String,
     image: String,
     title: String,
+    description: String,
 });
 const Book = mongoose.model("Book", bookSchema);
 
